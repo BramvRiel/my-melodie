59f8129cba4713ad8bc8ff1a04f4390b631e7e97
bram-nextgen <141123894+boqo-nextgen@users.noreply.github.com>
2023-08-01 15:25:28 +0200

    restart

--- portaal/src/app/app.component.spec.ts
+++ portaal/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
   }));
 
   it('should create the app', () => {
@@ -16,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Melodie'`, () => {
+  it(`should have as title 'portaal'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Melodie');
+    expect(app.title).toEqual('portaal');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Melodie app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('portaal app is running!');
   });
 });
